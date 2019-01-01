28bd6bf4d3a8341dcd606e96ae4cf564700f40f4
Dante <494941020@qq.com>
2019-01-01 15:29:03 +0800

    ajax获取数据

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { connect } from 'react-redux'; 
 import { CSSTransition } from 'react-transition-group';
 import { actionCreators } from './store';
@@ -19,78 +19,82 @@ import {
 	SearchInfoList
 }from './style.js';
 
-const getListArea = (show) => {
-	if (show){
+class Header extends Component {
+	getListArea() {
+		if (this.props.focused){
+			return (
+				<SearchInfo>
+					<SearchInfoTitle>
+						热门搜索
+						<SearchInfoSwitch>换一批</SearchInfoSwitch>
+					</SearchInfoTitle>
+					<SearchInfoList>
+						{
+							this.props.list.map((item) => {
+								return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+							})
+						}
+					</SearchInfoList>
+				</SearchInfo>
+			)
+		}else {
+			return null;
+		}
+	}
+
+	render() {
 		return (
-			<SearchInfo>
-				<SearchInfoTitle>
-					热门搜索
-					<SearchInfoSwitch>换一批</SearchInfoSwitch>
-				</SearchInfoTitle>
-				<SearchInfoList>
-					<SearchInfoItem>教育</SearchInfoItem>
-					<SearchInfoItem>区块链</SearchInfoItem>
-					<SearchInfoItem>小程序</SearchInfoItem>
-					<SearchInfoItem>vue</SearchInfoItem>
-					<SearchInfoItem>PHP</SearchInfoItem>
-					<SearchInfoItem>故事</SearchInfoItem>
-				</SearchInfoList>
-			</SearchInfo>
-		)
-	}else {
-		return null;
+			<HeaderWrapper>
+				<GlobalLogo />
+				<Logo />
+				<Nav>
+					<NavItem className='left active'>首页</NavItem>
+					<NavItem className='left'>下载App</NavItem>
+					<NavItem className='right'>登录</NavItem>
+					<NavItem className='right'>
+						<i className='iconfont'>&#xe636;</i>
+					</NavItem>
+					<SearchWrapper>
+						<CSSTransition
+							timeout={200}
+							in={this.props.focused}
+							classNames="slide"
+						>
+							<NavSearch
+								className={this.props.focused ? 'focused':''}
+								onFocus={this.props.handleInputFocus}
+								onBlur={this.props.handleInputBlur}
+							></NavSearch>
+						</CSSTransition>
+						<i className={this.props.focused ? 'focused iconfont':'iconfont'}>&#xe6dd;</i>
+						{this.getListArea(this.props.focused)}
+					</SearchWrapper>
+				</Nav>
+				<Addition>
+					<Button className='writing'>
+						<i className='iconfont'>&#xe6a4;</i>
+						写文章
+					</Button>
+					<Button className='reg'>注册</Button>
+				</Addition>
+			</HeaderWrapper>
+		)	
 	}
 }
 
-const Header = (props) => {
-	return (
-		<HeaderWrapper>
-			<GlobalLogo />
-			<Logo />
-			<Nav>
-				<NavItem className='left active'>首页</NavItem>
-				<NavItem className='left'>下载App</NavItem>
-				<NavItem className='right'>登录</NavItem>
-				<NavItem className='right'>
-					<i className='iconfont'>&#xe636;</i>
-				</NavItem>
-				<SearchWrapper>
-					<CSSTransition
-						timeout={200}
-						in={props.focused}
-						classNames="slide"
-					>
-						<NavSearch
-							className={props.focused ? 'focused':''}
-							onFocus={props.handleInputFocus}
-							onBlur={props.handleInputBlur}
-						></NavSearch>
-					</CSSTransition>
-					<i className={props.focused ? 'focused iconfont':'iconfont'}>&#xe6dd;</i>
-					{getListArea(props.focused)}
-				</SearchWrapper>
-			</Nav>
-			<Addition>
-				<Button className='writing'>
-					<i className='iconfont'>&#xe6a4;</i>
-					写文章
-				</Button>
-				<Button className='reg'>注册</Button>
-			</Addition>
-		</HeaderWrapper>
-	)
-}
 
 
 const mapStateToProps = (state) => {
 	return {
-		focused:state.getIn(['header','focused'])
+		focused:state.getIn(['header','focused']),
+		list: state.getIn(['header','list'])
 		// focused: state.get('header').get('focused')
 	}
 }
 const mapDispatchToProps = (dispatch) => {
 	 return {
 	 	handleInputFocus() {
+	 		dispatch(actionCreators.getList());
 	 		dispatch(actionCreators.searchFocus());
 	 	},
 	 	handleInputBlur() {
--- jianshu/src/common/header/store/reducer.js
+++ jianshu/src/common/header/store/reducer.js
@@ -2,15 +2,19 @@ import * as actionTypes from './actionTypes';
 import{ fromJS } from 'immutable';
 
 const defaultState = fromJS({
- 	focused:false
+ 	focused:false,
+ 	list:[]
 });
 
 export default (state= defaultState,action) => {
 	if (action.type === actionTypes.SEARCH_FOCUS){
 		return state.set('focused',true)
 	}
-	if ( action.type === actionTypes.SEARCH_BLUR) {
+	if (action.type === actionTypes.SEARCH_BLUR) {
 		return state.set('focused',false)
+	}
+	if (action.type === actionTypes.CHANGE_LIST) {
+		return state.set('list',action.data);
 	}
 	return state;
 
