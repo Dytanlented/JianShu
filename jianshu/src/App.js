7fd2f1f11f0d4a149f8749c7c7ab9ec9d175972d
Dante <494941020@qq.com>
2019-01-02 22:27:29 +0800

    router

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Header from './common/header/index.js';
 import { Provider } from 'react-redux';		
 import store from './store/index.js';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
     	<Provider store={store}>	
-      		<Header />
+      		<div>
+	      		<Header />
+	      		<BrowserRouter>
+	      			<div>
+		      			<Route path='/' exact render={()=><div>home</div>}></Route>
+		      			<Route path='/detail' exact render={()=><div>detail</div> }></Route>
+	      			</div>
+	      		</BrowserRouter>
+      		</div>
       	</Provider>
     );
   }
