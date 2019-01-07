d2f30bcd659fc5c5d4040880d06dff087fd03162
Dante <494941020@qq.com>
2019-01-07 15:27:13 +0800

    redux详情页面

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { DetailWrapper, Header, Content } from './style';
+import { connect } from 'react-redux';
  
 class Detail extends Component {
 	render() {
 		return (
 			<DetailWrapper>
-				<Header>
-					陈鲁豫，娱乐圈先知般的存在……
-				</Header>
-				<Content>
-					<img src='//upload-images.jianshu.io/upload_images/11490841-b803e2fe96102d62.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/367/format/webp'/>
-					<p>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</p>
-					<p><b>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</b></p>
-					<p>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</p>
-				</Content>
+				<Header>{this.props.title}</Header>
+				<Content 
+					 dangerouslySetInnerHTML={{__html:this.props.content}}
+				/>
 			</DetailWrapper>
 		)
 	}
 }
 
-export default Detail;
+const mapState = (state) => ({
+	title: state.getIn(['detail','title']),
+	content: state.getIn(['detail','content'])
+})
+
+export default connect(mapState,null)(Detail);
