3186420f9f803b26a1c23e34dc8a2a995bbece75
Dante <494941020@qq.com>
2019-01-07 15:06:46 +0800

    detail

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-
+import { DetailWrapper, Header, Content } from './style';
+ 
 class Detail extends Component {
 	render() {
 		return (
-			<div>Detail</div>
+			<DetailWrapper>
+				<Header>
+					陈鲁豫，娱乐圈先知般的存在……
+				</Header>
+				<Content>
+					<img src='//upload-images.jianshu.io/upload_images/11490841-b803e2fe96102d62.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/367/format/webp'/>
+					<p>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</p>
+					<p><b>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</b></p>
+					<p>陈鲁豫，好像有一种天生的能力，那就是，她采访谁，谁就倒霉。</p>
+				</Content>
+			</DetailWrapper>
 		)
 	}
 }
